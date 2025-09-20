6f47d409cc11a23545a3e0a091419ec9a119c31d
Amit <amitsahoo718@gmail.com>
2025-09-21 00:32:47 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,9 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+        System.out.println("Name:Amit");
+        System.out.println("Roll NO;58");
+        System.out.println("Branch:CSE");
+        System.out.println("College: Silicon Unversity");
+    }
+}
